941259897925762067789b5928dc5bb2930da969
Thomas Kirby <45469471+TomKirbz@users.noreply.github.com>
2019-10-09 11:14:35 +0100

    deleted keys.js

--- keys.js
+++
@@ -1,7 +0,0 @@
-const locationApi = "pk.eyJ1IjoidG9ta2lyYnkiLCJhIjoiY2swbnFpOTB6MDBsdTNsbjZmaWR3d2k1YiJ9.qXK_Mqd4z6MMDd-OtRMedg" 
-const apiKey = "adfc6c84f15b2a42759c68277dfb1ef0" 
-
-module.exports = {
-    locationApi,
-    apiKey
-}
